c55ba1eaca1413ec775f72d5cc5c446dd1b3be4f
Cao Tan Nguyen <tancao.nguyen2@dxc.com>
2026-07-13 15:30:47 +0700

    update UI box page.

--- DUCKSolution/DUCKSolution/Models/Box.cs
+++ DUCKSolution/DUCKSolution/Models/Box.cs
@@ -8,6 +8,16 @@ namespace DUCKSolution.Models
         public int Id { get; set; }
         public int STT { get; set; } 
         public int BoxNubmer { get; set; } = 0;
+        public int BoxCode1 { get; set; } = 0;
+
+        public int BoxCode2 { get; set; } = 0;
+
+        public int BoxCode3 { get; set; } = 0;
+
+        public int BoxCode4 { get; set; } = 0;
+
+        public int BoxCode5 { get; set; } = 0;
+
         public decimal BoxWeight { get; set; }  = 0;
 
         [Required]
--- DUCKSolution/DUCKSolution/Program.cs
+++ DUCKSolution/DUCKSolution/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddAntiforgery(options => options.HeaderName = "RequestVerificationToken");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
